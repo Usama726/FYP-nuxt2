16b78fba4e52ac8584a25624aa8b88bd2afd189d
usmanarif460 <usmanarif460@gmail.com>
2023-06-16 12:10:51 +0500

    fixed cart error

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,36 +1,29 @@
 
 <template>
   <!-- Main navigation container -->
-
   <div>
-
-    <nav class="fixed w-full h-16 nav-bg z-40 py-3 px-5">
+    <nav class="fixed z-40 w-full h-16 px-5 py-3 nav-bg">
       <div>
         <!-- navbar -->
-        <div class="flex justify-between align-middle w-full ">
+        <div class="flex justify-between w-full align-middle ">
           <!-- Header logo -->
           <div>
             <nuxt-link to="/"><img src="~assets/images/logo.png" class="logo-img"></nuxt-link>
           </div>
           <!-- Navbar -->
           <div class="hidden md:block">
-            <ul class=" pt-2 flex justify-center uppercase align-center space-x-8 text-md  text-white ">
+            <ul class="flex justify-center pt-2 space-x-8 text-white uppercase align-center text-md">
               <li><nuxt-link to="/" class="inline-block hover:scale-110">Home</nuxt-link></li>
               <li><nuxt-link to="/aboutUs" class="inline-block hover:scale-110">About </nuxt-link></li>
               <li><nuxt-link to="/medicines" class="inline-block hover:scale-110">Medicines</nuxt-link></li>
               <li><nuxt-link to="/contactUs" class="inline-block hover:scale-110">Contact </nuxt-link></li>
             </ul>
           </div>
-
-
-
           <!-- Mobile toggle -->
           <div class="flex gap-3">
-
-
             <div class="relative">
-              <nuxt-link to="/checkout ">
-                <svg class="h-9 w-9 cursor-pointer " viewBox="0 0 32 32" id="svg5" version="1.1"
+              <button @click="modalIsOpen = true">
+                <svg class="cursor-pointer h-9 w-9 " viewBox="0 0 32 32" id="svg5" version="1.1"
                   xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg" fill="#ffffff">
                   <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                   <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
@@ -55,10 +48,12 @@
                     </g>
                   </g>
                 </svg>
-                <!-- <p class="absolute top-0 left-4 text-white font-bold bg-red-500 px-1 rounded-full">{{ cart.length }}</p> -->
-              </nuxt-link>
+                <p class="absolute top-0 px-1 text-sm font-bold text-white bg-red-500 rounded-full left-5">{{
+                  cart.length
+                }}</p>
+              </button>
             </div>
-            <div class="hidden md:block space-x-2" v-if="!user">
+            <div class="hidden space-x-2 md:block" v-if="!user">
               <headerbtn>
                 <nuxt-link to="/login">Login</nuxt-link>
               </headerbtn>
@@ -74,7 +69,7 @@
 
             <div class="md:hidden">
               <button @click="drawer">
-                <svg class="h-9 w-9 fill-current text-white" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                <svg class="text-white fill-current h-9 w-9" fill="none" stroke-linecap="round" stroke-linejoin="round"
                   stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                   <path d="M4 6h16M4 12h16M4 18h16"></path>
                 </svg>
@@ -83,22 +78,20 @@
 
           </div>
         </div>
-
-
         <!-- Dark Background Transition -->
         <transition enter-class="opacity-0" enter-active-class="ease-out transition-medium" enter-to-class="opacity-100"
           leave-class="opacity-100" leave-active-class="ease-out transition-medium" leave-to-class="opacity-0">
-          <div @keydown.esc="isOpen = false" v-show="isOpen" class="z-10 fixed inset-0 transition-opacity">
+          <div @keydown.esc="isOpen = false" v-show="isOpen" class="fixed inset-0 z-10 transition-opacity">
             <div @click="isOpen = false" class="absolute inset-0 bg-black opacity-50" tabindex="0"></div>
           </div>
         </transition>
 
         <!-- Drawer Menu -->
         <aside
-          class=" transform top-0 left-0 w-full h-screen bg-white fixed  overflow-auto ease-in-out transition-all duration-300 z-30"
+          class="fixed top-0 left-0 z-30 w-full h-screen overflow-auto transition-all duration-300 ease-in-out transform bg-white "
           :class="isOpen ? 'translate-x-0' : '-translate-x-full'">
 
-          <div class="flex align-middle justify-between  nav-bg px-3 py-2">
+          <div class="flex justify-between px-3 py-2 align-middle nav-bg">
             <div>
               <img src="~assets/images/logo.png" class="logo-img">
             </div>
@@ -114,36 +107,36 @@
 
 
           <ul
-            class="container mx-auto p-5 uppercase divide-y text-md font-bold md:text-xl flex flex-col align-center justify-center">
-            <li><nuxt-link to="/" @click="isOpen = false" class="my-4 block text-center hover:scale-110 ">Home</nuxt-link>
+            class="container flex flex-col justify-center p-5 mx-auto font-bold uppercase divide-y text-md md:text-xl align-center">
+            <li><nuxt-link to="/" @click="isOpen = false" class="block my-4 text-center hover:scale-110 ">Home</nuxt-link>
             </li>
             <li><nuxt-link to="/aboutus" @click="isOpen = false"
-                class="my-4 block text-center hover:scale-110">About</nuxt-link></li>
+                class="block my-4 text-center hover:scale-110">About</nuxt-link></li>
             <li><nuxt-link to="/medicines" @click="isOpen = false"
-                class="my-4 block text-center hover:scale-110">Medicines</nuxt-link></li>
+                class="block my-4 text-center hover:scale-110">Medicines</nuxt-link></li>
             <li><nuxt-link to="/contactus" @click="drawer"
-                class="my-4 block text-center hover:scale-110">Contact</nuxt-link></li>
+                class="block my-4 text-center hover:scale-110">Contact</nuxt-link></li>
             <li v-if="!user" class="flex justify-center gap-3">
               <nuxt-link to="/login" @click="isOpen = false"
-                class="my-5  text-center font-semibold cta inline-block text-blue-600 border-2 border-blue-500 hover:bg-blue-600 px-3 py-2 rounded hover:text-white">Login</nuxt-link>
+                class="inline-block px-3 py-2 my-5 font-semibold text-center text-blue-600 border-2 border-blue-500 rounded cta hover:bg-blue-600 hover:text-white">Login</nuxt-link>
               <nuxt-link to="/signup" @click="isOpen = false"
-                class="my-5  text-center font-semibold cta inline-block bg-blue-500 hover:bg-blue-600 px-3 py-2 rounded text-white">Sign
+                class="inline-block px-3 py-2 my-5 font-semibold text-center text-white bg-blue-500 rounded cta hover:bg-blue-600">Sign
                 Up</nuxt-link>
             </li>
             <li v-else class="flex justify-center ">
               <button @click="logoutbtn"
-                class="my-5 text-center font-semibold cta inline-block text-blue-600 border-2 border-blue-500 hover:bg-blue-600 px-3 py-2 rounded hover:text-white">Sign
+                class="inline-block px-3 py-2 my-5 font-semibold text-center text-blue-600 border-2 border-blue-500 rounded cta hover:bg-blue-600 hover:text-white">Sign
                 Out
               </button>
             </li>
           </ul>
 
           <div class="follow ">
-            <p class="italic font-bold text-xl sm:text-md text-center">follow us:</p>
-            <div class="flex align-center justify-center gap-16  mt-6 ">
+            <p class="text-xl italic font-bold text-center sm:text-md">follow us:</p>
+            <div class="flex justify-center gap-16 mt-6 align-center ">
               <div>
                 <a href="#!">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-full w-8 hover:scale-110  " fill="currentColor"
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-full mx-auto hover:scale-110 " fill="currentColor"
                     viewBox="0 0 24 24">
                     <path
                       d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" />
@@ -153,7 +146,7 @@
               </div>
               <div>
                 <a href="#!">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-full w-8 hover:scale-110" fill="currentColor"
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-full mx-auto hover:scale-110" fill="currentColor"
                     viewBox="0 0 24 24">
                     <path
                       d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z" />
@@ -162,7 +155,7 @@
               </div>
               <div>
                 <a href="#!">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-full w-8 hover:scale-110" fill="currentColor"
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-8 h-full mx-auto hover:scale-110" fill="currentColor"
                     viewBox="0 0 24 24">
                     <path
                       d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
@@ -178,23 +171,53 @@
       </div>
     </nav>
 
+    <div v-if="modalIsOpen"
+      class="fixed top-0 right-0 z-40 flex items-end justify-end w-full h-screen mr-auto transition-opacity bg-black bg-opacity-50 sm:block">
+      <div
+        class="relative flex items-center justify-center w-full h-full ml-auto bg-white rounded-md md:w-5/12 xl:w-3/12">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="absolute w-6 h-6 cursor-pointer top-2 right-2" @click="modalIsOpen = false">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+        <div v-for="(product, index) in cart" :key="index"
+          class="flex items-center pr-2 space-x-4 border-2 border-blue-400 rounded-lg">
+          <div class="w-36 h-36">
+            <img :src="product.imageUrl">
+          </div>
+          <div>
+            <h2 class="mb-3 font-bold text-md">{{ product.name }}</h2>
+
+            <span class="text-black "> PKR {{ product.price }} .00</span>
+          </div>
+          <div>
+            <button @click="removeItem">
+              <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import Headerbtn from './headerbtn.vue';
-
+// import Headerbtn from './headerbtn.vue';
 export default {
   data() {
     return {
       user: '',
       isOpen: false,
-      // cart: [],
+      cart: [],
+      modalIsOpen: false
     };
   },
-  // created() {
-  //   this.cart = JSON.parse(localStorage.getItem("cart_storage") || '[]')
-  // },
+  async created() {
+    this.cart = await JSON.parse(localStorage.getItem("cart_storage"))
+  },
   methods: {
     drawer() {
       this.isOpen = !this.isOpen;
@@ -204,6 +227,11 @@ export default {
       this.$fire.auth.signOut();
       this.user = '';
       this.$router.push("/");
+    },
+    removeItem(index) {
+      this.cart.splice(index, 1)
+      localStorage.setItem("cart_storage", JSON.stringify(this.cart))
+
     }
   },
   watch: {
---
+++ pages/test.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="fixed top-0 right-0 z-40 flex items-end justify-end w-full h-screen mr-auto transition-opacity bg-black bg-opacity-50 sm:block">
+        <div
+            class="relative flex items-center justify-center w-full h-full ml-auto bg-white rounded-md md:w-9/12 xl:w-4/12">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                class="absolute w-6 h-6 top-2 right-2">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+            <!-- <div v-for="(product, index) in cart" :key="index"
+                class="flex items-center pr-2 space-x-4 border-2 border-blue-400 rounded-lg">
+                <div class="w-36 h-36">
+                    <img :src="product.imageUrl">
+                </div>
+                <div>
+                    <h2 class="mb-3 font-bold text-md">{{ product.name }}</h2>
+
+                    <span class="text-black "> PKR {{ product.price }} .00</span>
+                </div>
+                <div>
+                    <button @click="removeItem">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            </div> -->
+            Hello
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
