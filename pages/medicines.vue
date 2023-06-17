a368c97e9a9bd897b9eb75ab500339eeeba12e46
usmanarif460 <usmanarif460@gmail.com>
2023-06-17 23:22:32 +0500

    fixed styling

--- pages/medicines.vue
+++ pages/medicines.vue
@@ -4,11 +4,12 @@
     <top-svg />
     <!-- Top Banner Of Every Page -->
     <top-header>Get your All <span class="text-red-800 ">Medicines+</span><br>From Here</top-header>
-    <div class="mt-[180px] mb-24 flex justify-around flex-wrap gap-4">
-      <the-product :product="product"  v-for="(product, index) in products" :key="`${product.id}-product-${index}`">
+    <div
+      class="mt-[180px] mb-24 mx-auto w-11/12 grid grid-cols-1 md:grid-cols-3 lg:grid-col-4 xl:grid-cols-5 2xl:grid-cols-6 gap-10">
+      <the-product :product="product" v-for="(product, index) in products" :key="`${product.id}-product-${index}`">
       </the-product>
     </div>
-    <thefooter />
+    <thefooter class="" />
   </div>
 </template>
 
@@ -35,6 +36,6 @@ export default {
       this.products = items
     })
   },
- 
+
 };
 </script>
